8e3b9c3a257fdf0a1a28958ff927d2a4651bb142
Simeon Franklin <simeonf@gmail.com>
2023-01-02 09:09:58 -0800

    Use ec2 settings in wsgi file

--- readaloud/readaloud/wsgi.py
+++ readaloud/readaloud/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readaloud.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readaloud.settings_ec2")
 
 application = get_wsgi_application()
